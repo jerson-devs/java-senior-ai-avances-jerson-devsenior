abd10d22d6a802f179f7ba84d8ae0b020978a426
jgaete <gerson.gaete@gmail.com>
2025-12-03 18:43:29 -0300

    feat: Completa ejercicio 1 Clase 3 - Positivo/Negativo/Cero

---
+++ modulo1/clase3/ejercicios_adicionales/src/Ejercicios01.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Ejercicios01 {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+
+        double numReal = 0;
+
+        System.out.print("Ingrese un numero real (ej: 1.5): ");
+        numReal = sc.nextDouble();
+
+        if (numReal == 0) {
+            System.out.println("El numero {" + numReal + "} ingresado es cero.");
+        } else if (numReal > 0) {
+            System.out.println("El numero {" + numReal + "} ingresado es positivo.");
+        } else {
+            System.out.println("El numero {" + numReal + "} ingresado es negativo.");
+        }
+
+        sc.close();
+    }
+}
